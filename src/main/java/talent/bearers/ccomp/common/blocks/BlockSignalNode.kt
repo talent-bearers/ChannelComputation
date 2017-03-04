3f0906bac8e61156d19a2879ebc4433ab0077e91
yrsegal <yrsegal@gmail.com>
2017-03-04 00:13:01 -0500

    analog signals

--- src/main/java/talent/bearers/ccomp/common/blocks/BlockSignalNode.kt
+++ src/main/java/talent/bearers/ccomp/common/blocks/BlockSignalNode.kt
@@ -1,9 +1,16 @@
 package talent.bearers.ccomp.common.blocks
 
+import com.teamwizardry.librarianlib.common.base.block.TileMod
+import com.teamwizardry.librarianlib.common.util.autoregister.TileRegister
+import com.teamwizardry.librarianlib.common.util.saving.Save
+import net.minecraft.block.state.IBlockState
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import talent.bearers.ccomp.api.packet.IPacket
+import talent.bearers.ccomp.common.packets.SignalPacket
 
 /**
  * @author WireSegal
@@ -11,14 +18,31 @@ import talent.bearers.ccomp.api.packet.IPacket
  */
 class BlockSignalNode : BlockBaseNode("signal_node") {
     override fun requestReadPacket(packetType: String, strength: Int, pos: BlockPos, world: IBlockAccess): IPacket? {
-        return null //todo
+        if (packetType != "signal") return null
+        return SignalPacket(world.getStrongPower(pos, connectionPoint(pos, world)))
     }
 
     override fun requestPullPacket(packetType: String, strength: Int, pos: BlockPos, world: World) = null
 
     override fun pushPacket(packet: IPacket, pos: BlockPos, world: World): IPacket? {
         if (packet.type != "signal") return packet
-        //todo signal holding
+        val tile = world.getTileEntity(pos) as? TileSignalNode
+        tile?.signal = SignalPacket.getStrength(packet).toByte()
+        world.notifyNeighborsOfStateChange(pos, this)
+        tile?.markDirty()
         return null
     }
+
+    override fun getStrongPower(blockState: IBlockState, blockAccess: IBlockAccess, pos: BlockPos, side: EnumFacing?)
+            = getWeakPower(blockState, blockAccess, pos, side)
+    override fun getWeakPower(blockState: IBlockState, blockAccess: IBlockAccess, pos: BlockPos, side: EnumFacing?)
+            = if (side == connectionPoint(pos, blockAccess))
+                (blockAccess.getTileEntity(pos) as? TileSignalNode)?.signal?.toInt() ?: 0
+            else 0
+
+    override fun hasTileEntity(state: IBlockState?) = true
+
+    override fun createTileEntity(world: World, state: IBlockState) = TileSignalNode()
+
+    @TileRegister("signal_node") class TileSignalNode(@Save var signal: Byte = 0) : TileMod()
 }
